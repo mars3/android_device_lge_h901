e81c31697ecad6b95a86cf9ff9d1056dcfb1c21b
mars3 <urkumbaevm@gmail.com>
2016-03-13 12:25:07 +0600

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/h901/full_h901.mk)
+$(call inherit-product, device/lge/h961n/full_h961n.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -7,10 +7,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 
-PRODUCT_NAME := cm_h901
+PRODUCT_NAME := cm_h961n
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="pplus" \
     PRODUCT_NAME="pplus_global_com" \
-    BUILD_FINGERPRINT="lge/pplus_tmo_us/pplus:5.1.1/LMY47V/152801355bef0:user/release-keys" \
+    BUILD_FINGERPRINT="lge/pplus_global_com/pplus:5.1.1/LMY47V/152801355bef0:user/release-keys" \
     PRIVATE_BUILD_DESC="pplus_global_com-user 5.1.1 LMY47V 152801355bef0 release-keys"
